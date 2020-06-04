0a7e3c97fe67071037174ecbc4cd1bab960f5c8b
Sébastien Canonica <sebcano.biz@laposte.net>
2020-06-04 12:41:27 +0200

    With Either

--- java/src/main/java/Pipeline.java
+++ java/src/main/java/Pipeline.java
@@ -2,6 +2,7 @@ import dependencies.Config;
 import dependencies.Emailer;
 import dependencies.Logger;
 import dependencies.Project;
+import io.vavr.control.Either;
 
 public class Pipeline {
     private final Config config;
@@ -14,68 +15,45 @@ public class Pipeline {
         this.log = log;
     }
 
-    interface Notifier {
-        void sendNotification(String message);
-    }
-
-    class NotifierFactory {
-        Notifier build(Config config) {
-            if (config.sendEmailSummary()) return new EmailNotifier();
-            else return new NullNotifier();
-        }
-
-        class NullNotifier implements Notifier {
-            public void sendNotification(String message) {
-                log.info("Email disabled");
-            }
-        }
-
-        class EmailNotifier implements Notifier {
-            public void sendNotification(String message) {
-                log.info("Sending email");
-                emailer.send(message);
-            }
-        }
-    }
-
     public void run(Project project) {
-        /*
-            hT -V-> rT -V-> d -V-> sES -V-> m1
-                                       -F-> lI
-                              -F-> sES -V-> m2
-                                       -F-> lI
-                       -F->        sES -V-> m3
-                                       -F-> lI
-               -F->         d -V-> sES -V-> m1
-                                       -F-> lI
-                              -F-> sES -V-> m2
-                                       -F-> lI
-         */
+        test(project)
+                .flatMap(this::deploy)
+                .map(p -> "Deployment completed successfully")
+                .bimap(this::notify, this::notify);
+    }
 
-        String status;
+    private Either<String, Project> test(Project project) {
         if (project.hasTests()) {
             if ("success".equals(project.runTests())) {
                 log.info("Tests passed");
-                status = deploy(project);
+                return Either.right(project);
             } else {
                 log.error("Tests failed");
-                status = "Tests failed";
+                return Either.left("Tests failed");
             }
         } else {
             log.info("No tests");
-            status = deploy(project);
+            return Either.right(project);
         }
-
-        new NotifierFactory().build(config).sendNotification(status);
     }
 
-    private String deploy(Project project) {
+    private Either<String,Project> deploy(Project project) {
         if ("success".equals(project.deploy())) {
             log.info("Deployment successful");
-            return "Deployment completed successfully";
+            return Either.right(project);
         } else {
             log.error("Deployment failed");
-            return "Deployment failed";
+            return Either.left("Deployment failed");
         }
+    }
+
+    private Void notify(String message) {
+        if (config.sendEmailSummary()) {
+            log.info("Sending email");
+            emailer.send(message);
+        } else {
+            log.info("Email disabled");
+        }
+        return null;
     }
 }
